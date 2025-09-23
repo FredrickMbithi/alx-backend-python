bd0e2b27f8fbc675ac93d32cf72954642c3b11ea
FredrickMbithi <FredrickMbithi000@gmail.com>
2025-09-23 20:16:11 +0300

    Update API viewsets, filters, and routing for conversations and messages

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,10 +1,10 @@
 # chats/urls.py
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import UserViewSet, ConversationViewSet, MessageViewSet
 
-# Create DRF router
-router = DefaultRouter()
+# Create DRF router using routers.DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
