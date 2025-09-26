786feb0a409d310d336cbaac5922f9b87968abbd
FredrickMbithi <FredrickMbithi000@gmail.com>
2025-09-26 22:28:22 +0300

    Update settings.py to use JWT authentication and default permissions

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -10,3 +10,23 @@ class IsOwner(BasePermissions):
         if hasattr(obj, "participants"):
             return request.user in obj.participants.all()
         return False
+
+
+from rest_framework.permissions import BasePermission
+
+class IsParticipantOfConversation(BasePermission):
+    """
+    Custom permission:
+    - Allow only participants of a conversation to view, send, update, or delete messages.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Case 1: If object is a Conversation
+        if hasattr(obj, "participants"):
+            return request.user in obj.participants.all()
+
+        # Case 2: If object is a Message
+        if hasattr(obj, "conversation"):
+            return request.user in obj.conversation.participants.all()
+
+        return False
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,43 +1,30 @@
-from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.response import Response
-from django_filters.rest_framework import DjangoFilterBackend
-from .models import Message, Conversation, User
-from .serializers import MessageSerializer, ConversationSerializer, UserSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
-
+from .permissions import IsParticipantOfConversation
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['participants']
 
     def get_queryset(self):
         # Only return conversations where the logged-in user is a participant
-        return Conversation.objects.filter(participants=self.request.user)
-
-    def perform_create(self, serializer):
-        # Always add the logged-in user as a participant
-        conversation = serializer.save()
-        conversation.participants.add(self.request.user)
+        return Conversation.objects.filter(participants=self.request.user).prefetch_related('participants', 'messages')
 
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['conversation']
 
     def get_queryset(self):
-        # Only return messages in conversations the logged-in user is part of
+        # Only return messages from conversations the user is a participant in
         return Message.objects.filter(conversation__participants=self.request.user)
 
-    def perform_create(self, serializer):
-        # Always set sender = logged-in user
-        serializer.save(sender=self.request.user)
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['sender'] = str(request.user.user_id)  # Force sender = logged in user
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        message = serializer.save()
+        return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
