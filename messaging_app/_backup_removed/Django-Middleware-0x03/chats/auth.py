d9e169e27472a129fb1dd67bfe8d4be81b84dcc2
FredrickMbithi <FredrickMbithi000@gmail.com>
2025-10-25 02:09:54 +0300

    chore: remove obsolete _backup_removed folder

--- messaging_app/_backup_removed/Django-Middleware-0x03/chats/auth.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """
-    Custom JWT Authentication.
-    You can extend this later with extra checks.
-    """
-    def authenticate(self, request):
-        return super().authenticate(request)
